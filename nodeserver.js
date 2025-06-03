ef82e28c5dfd92390f5fcd0d53f2ded69abe7eb2
unknown <prerna27imp10@gmail.com>
2025-06-03 09:04:46 +0530

    different http req-res

--- nodeserver.js
+++ nodeserver.js
@@ -1,14 +1,39 @@
 const http = require("http")
 
-const server = http.createServer((req,res) =>{    //1- creating server using http module. which takes one callback having two arguments : req,res
-    // console.log(req) // see the request body in console, 
-    //************When went to brower it will not open any page as we haven't send any reponse */
-    console.log("No response",res)
-//http message :- startline,header, and the body. we don't need to write startline
-//3- creating response 
-    res.writeHead(200, {'content-type' : "text/html"}) //1- writeHead takes two agrs one is the statuscode and the other is k-v pair
-    res.write('<h1>Hello world!</h1>') //2- the html text
-    res.end() // 3- to stop the connections
+const fs = require("fs") // fs file system module jai jo iss folder mai html css js images ko read krne mai help karega
+
+//Note : that the html and the css file and the external files
+
+const server = http.createServer((req,res) =>{   
+    if(req.url === "/") {
+        // console.log(req.url)
+        //1- reading html file
+        res.writeHead(200, {'content-type' : "text/html"}) 
+        const readHtml = fs.readFileSync('./node.html')
+        res.write(readHtml);
+        res.end();
+    }
+    //2- usecase for reading image
+    else if(req.url === "/bp.jpg"){
+        res.writeHead(200, {'content-type' : "img/jpg"}) 
+        const readImg = fs.readFileSync('./bp.jpg')
+        res.write(readImg);
+        res.end();
+    }
+    //3- is for reading css file.
+    else if(req.url === "/node.css"){
+        res.writeHead(200, {'content-type' : "text/css"}) 
+        const readCSS = fs.readFileSync('./node.css')
+        res.write(readCSS);
+        res.end();
+    }
+    else{
+        res.writeHead(404, {'content-type' : "text/html"});
+        res.write('<h4>Sorry the page you are looking for dont exist</h4>')
+        res.end()
+    }
+
+    
 })
 
 //2- need to have a port to listen
